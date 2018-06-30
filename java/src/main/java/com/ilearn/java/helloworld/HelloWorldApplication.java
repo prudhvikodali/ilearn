8246bd508f01120af6c9871ac87dd44baa32e5bc
PRUDHVI KODALI <prudhvikodali@gmail.com>
2018-06-30 17:38:23 +0530

    Java Hello World

---
+++ java/src/main/java/com/ilearn/java/helloworld/HelloWorldApplication.java
@@ -0,0 +1,7 @@
+package com.ilearn.java.helloworld;
+
+public class HelloWorldApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");   // Prints the string to the console.
+    }
+}
