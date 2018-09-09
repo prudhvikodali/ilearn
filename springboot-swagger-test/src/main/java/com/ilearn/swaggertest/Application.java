1f535b96158f0b06b4b2fb9ad5368b24b6e88e24
PRUDHVI KODALI <prudhvikodali@gmail.com>
2018-09-09 19:08:18 +0530

    samples

---
+++ springboot-swagger-test/src/main/java/com/ilearn/swaggertest/Application.java
@@ -0,0 +1,14 @@
+package com.ilearn.swaggertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+
+	}
+
+}
